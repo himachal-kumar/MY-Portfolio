d94980a5d20ae1112f2200187983e9aa66daaaa4
Himachal <himachalkumar75@gmail.com>
2025-01-09 12:16:16 +0530

    3

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
+    swcMinify: true, // Ensure SWC minification is enabled
   },
   images: {
     remotePatterns: [
